b6951b013981406e21e09088a0892d557c27b22a
João Victor Gomes <71712773+JoaoVictorgms@users.noreply.github.com>
2022-03-25 10:25:36 -0300

    Exemplo de Flow Layout

---
+++ FlowLayout/src/Exemplo.java
@@ -0,0 +1,23 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Exemplo extends JFrame{
+	public Exemplo() {
+		   JFrame frame = new JFrame();
+		   frame.setTitle("Minha janela");
+		   frame.setSize(400,400);
+		   frame.setLocation(400,400);
+		   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		   frame.setVisible(true);
+		   frame.setLayout(new FlowLayout());
+		   frame.add(new JButton("Campo 1"));
+		   frame.add(new JButton("Campo 2"));
+		   frame.add(new JButton("Campo 3"));
+		   frame.add(new JButton("Campo 4"));
+}
+public static void main(String[] args) {
+	 new Exemplo();
+}
+}
